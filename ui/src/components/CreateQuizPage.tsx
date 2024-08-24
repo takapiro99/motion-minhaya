77e1a916f87a5c2668333895860bdb2927182fd9
starmine7940 <starmine7940@gmail.com>
2024-08-24 16:44:48 +0900

    各ページコンポーネントにリンクを置いてみた

--- ui/src/components/CreateQuizPage.tsx
+++ ui/src/components/CreateQuizPage.tsx
@@ -1,7 +1,11 @@
 import { FC } from "react"
+import { Link } from "react-router-dom"
 
 export const CreateQuizPage: FC = () => {  
   return (
-    <div>This is a create quiz page.</div>
+    <div>
+      <Link to="/">Top</Link> | <Link to="/game">Game</Link> | <Link to="/create-quiz">CreateGame</Link>
+      <div>This is a create quiz page.</div>
+    </div>
   )
 }
--- ui/src/components/TopPage.tsx
+++ ui/src/components/TopPage.tsx
@@ -1,7 +1,11 @@
 import { FC } from "react"
+import { Link } from "react-router-dom"
 
 export const TopPage: FC = () => {  
   return (
-    <div>This is a top page.</div>
+    <div>
+      <Link to="/">Top</Link> | <Link to="/game">Game</Link> | <Link to="/create-quiz">CreateGame</Link>
+      <div>This is a top page.</div>
+    </div>
   )
 }
