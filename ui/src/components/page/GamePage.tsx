13cbcc324dfaee686474863a1e3cf169d7e71042
starmine7940 <starmine7940@gmail.com>
2024-08-25 17:59:34 +0900

    WaitingRoomUnjoinable の作成

--- ui/src/components/page/GamePage.tsx
+++ ui/src/components/page/GamePage.tsx
@@ -8,6 +8,7 @@ import { GameOngoing } from "../game/GameOngoing"
 import { GameFinished } from "../game/GameFinished"
 import { SocketContext } from "../../SocketContext"
 import { ToTopPageButton } from "../utils/toTopPageButton"
+import { WaitingRoomUnjoinable } from "../game/WaitingRoomUnjoinable"
 
 export const GamePage: FC = () => {
   const { gameStatus } = useContext(SocketContext)
@@ -19,6 +20,7 @@ export const GamePage: FC = () => {
       {gameStatus === "OUT_OF_GAME" && <div>異常な gameStatus です。</div>}
       {gameStatus === "NAME_INPUTING" && <NameInputing />}
       {gameStatus === "CONFIRMING_WAITING_ROOM_JOINABLE" && <ConfirmingWaitingRoomJoinable />}
+      {gameStatus === "WAITING_ROOM_UNJOINABLE" && <WaitingRoomUnjoinable />}
       {gameStatus === "PARTICIPANTS_WAITING" && <ParticipantsWaiting />}
       {gameStatus === "PARTICIPANTS_MATCHED" && <ParticipantsMatched />}
       {gameStatus === "GAME_STARTED" && <GameStarted />}
