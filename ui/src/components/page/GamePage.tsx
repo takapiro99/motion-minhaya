e2e4433b81decdacf000fffdab18162362ff9b5c
starmine7940 <starmine7940@gmail.com>
2024-08-25 18:50:32 +0900

    GamePage の異常なステータスの文言を修正

--- ui/src/components/page/GamePage.tsx
+++ ui/src/components/page/GamePage.tsx
@@ -17,7 +17,7 @@ export const GamePage: FC = () => {
     <div>
       <ToTopPageButton />
       <div>This is a game page.</div>
-      {gameStatus === "OUT_OF_GAME" && <div>異常な gameStatus です。</div>}
+      {gameStatus === "OUT_OF_GAME" && <div>異常な gameStatus です。TopPage に戻ってください。</div>}
       {gameStatus === "NAME_INPUTING" && <NameInputing />}
       {gameStatus === "CONFIRMING_WAITING_ROOM_JOINABLE" && <ConfirmingWaitingRoomJoinable />}
       {gameStatus === "WAITING_ROOM_UNJOINABLE" && <WaitingRoomUnjoinable />}
