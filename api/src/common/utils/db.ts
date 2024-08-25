16368b457c055ffe9712ade525beebd5e50de30a
starmine7940 <starmine7940@gmail.com>
2024-08-25 15:13:44 +0900

    api に ui でも使う Game 型を定義

--- api/src/common/utils/db.ts
+++ api/src/common/utils/db.ts
@@ -4,7 +4,55 @@ import { LowSync, MemorySync } from "./lowdb";
 // With this adapter, calling `db.write()` will do nothing.
 // One use case for this adapter can be for tests.
 
+export type Game = {
+  status: "NONE";
+  gameId: null;
+  participants: null;
+  currentQuizNumberOneIndexed: null;
+  quizzes: null;
+  gameResult: null;
+} | {
+  status: "WAITING_PARTICIPANTS";
+  gameId: string;
+  participants: {
+    connectionId: string | null;
+    clientId: string;
+    name: string;
+  }[];
+  currentQuizNumberOneIndexed: 1;
+  quizzes: null;
+  gameResult: null;
+} | {
+  status: "ONGOING";
+  gameId: string;
+  participants: {
+    connectionId: string | null;
+    clientId: string;
+    name: string;
+  }[];
+  currentQuizNumberOneIndexed: number;
+  quizzes: {
+    quizNumber: number;
+    motionId: string;
+    motionStartTimestamp: number; // unix timestamp
+    answerFinishTimestamp: number; // unix timestamp
+    guesses: {
+      clientId: string;
+      buttonPressedTimeMs: number;
+      similarityPoint: number; // 類似度点数
+      quizPoint: number; // この問題で得た点数
+    }[];
+  }[];
+  gameResult:
+    | {
+        clientId: string;
+        gamePoint: number;
+      }[]
+    | null;
+}
+
 type DataBase = {
+  // games: Game[], <- でいいかも？
   games: (
     | {
         status: "WAITING_PARTICIPANTS";
