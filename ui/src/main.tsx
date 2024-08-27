c044cb930fdd8c1321da0f9d3afb12824b67d1e5
Takahiro Nishino <bigstitch99@gmail.com>
2024-08-28 03:36:34 +0900

    録画のプロセス

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "semantic-ui-css/semantic.min.css";
+// import { StrictMode } from "react";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  // <StrictMode>
+  <App />
+  // </StrictMode>
+);
