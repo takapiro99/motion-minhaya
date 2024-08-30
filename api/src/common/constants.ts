05a9303d8a42a238447b26571513e50dfa421315
Takahiro Nishino <bigstitch99@gmail.com>
2024-08-31 08:19:00 +0900

    Update constants.ts

--- api/src/common/constants.ts
+++ api/src/common/constants.ts
@@ -1,19 +1,21 @@
 export const constants = {
-  PARTICIPANTS_PER_GAME: 2,
+  // 参加者
+  PARTICIPANTS_PER_GAME: 4, // 人
+
   // 1問ごとの回答時間（実質使ってない）
   ANSWER_TIME_MS: 400 * 1000,
 
   // みんな集まってからGameを始めるまで
-  PARTICIPANTS_GATHERING_START_GAME_MS: 3 * 1000,
+  PARTICIPANTS_GATHERING_START_GAME_MS: 1.5 * 1000,
 
   // クイズ1問目の開始まで
   START_GAME_TO_START_QUIZ1_MS: 3 * 1000,
 
   // みんなの回答が集まってから結果発表まで
-  ANSWERS_GATHERING_TO_QUIZ_RESULT_MS: 2 * 1000,
+  ANSWERS_GATHERING_TO_QUIZ_RESULT_MS: 1.5 * 1000,
 
   // 結果発表から次のクイズまで
   QUIZ_RESULT_TO_NEXT_QUIZ_MS: 7 * 1000,
 
-  NUMBER_OF_QUIZZES: 1,
+  NUMBER_OF_QUIZZES: 3,
 };
