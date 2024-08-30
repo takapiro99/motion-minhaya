ccaf8c9cb1f3e7e40efafc0aa6ea225f46433784
Takahiro Nishino <bigstitch99@gmail.com>
2024-08-31 01:54:28 +0900

    just toppage

---
+++ ui/src/components/createQuiz/TopPagePreview.tsx
@@ -0,0 +1,89 @@
+import React, { useRef } from "react";
+import { ThreeElements, useFrame } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
+import { Bone, Quaternion, Vector3, Vector3Like } from "three";
+
+export const TopPagePreview: React.FC<{}> = () => {
+  const gltf = useLoader(GLTFLoader, "/gltf/scene.gltf");
+  const modelRef = useRef<ThreeElements["primitive"]>(null);
+
+  const getBone = (num: number): Bone => {
+    if (!modelRef.current) return {} as Bone;
+    const names = [
+      "_rootJoint",
+      "root_01",
+      "spine_01_02",
+      "pelvis_03",
+      "thighL_04",
+      "shinL_05",
+      "footL_06",
+      "toeL_07",
+      "thighR_08",
+      "shinR_09",
+      "footR_010",
+      "toeR_011",
+      "spine_02_012",
+      "spine_03_013",
+      "head_014",
+      "shoulderL_015",
+      "upper_armL_016",
+      "forearmL_017",
+      "handL_018",
+      "fingersL_019",
+      "thumb_01L_020",
+      "thumb_02L_021",
+      "shoulderR_022",
+      "upper_armR_023",
+      "handR_024",
+      "fingersR_025",
+      "thumb_01R_026",
+      "thumb_02R_027",
+      "TAR_kneeL_028",
+      "IK_handL_029",
+      "IK_footL_030",
+      "TAR_elbowL_031",
+      "TAR_kneeR_032",
+      "IK_handR_033",
+      "IK_footR_034",
+      "TAR_elbowR_035",
+      "forearmR_00",
+    ];
+    return modelRef.current.getObjectByName(names[num]);
+  };
+
+  useFrame(() => {
+    // https://threejs.org/docs/#api/en/core/Object3D
+    if (modelRef.current) {
+      getBone(0).rotation.set(-1, 0, 0);
+      getBone(1).rotateZ(0.001);
+      getBone(1).rotateY(0.01);
+    }
+  });
+  // const envMap = useEnvironment({
+  //   files: "/gltf/background.hdr",
+  // });
+
+  return (
+    <>
+      {/* <Environment map={envMap} background /> */}
+      <ambientLight intensity={1} color={"#ffa0f0"} />
+      <spotLight
+        position={[10, 30, 30]}
+        angle={0.15}
+        penumbra={1}
+        decay={0}
+        intensity={5}
+        color={"#FF00F0"}
+      />
+      <pointLight
+        position={[-10, -10, -10]}
+        decay={0}
+        intensity={1}
+        color={"#ffa0f0"}
+      />
+      {/* <OrbitControls /> */}
+      <primitive ref={modelRef} object={gltf.scene} />
+    </>
+  );
+};
--- ui/src/components/page/TopPage.tsx
+++ ui/src/components/page/TopPage.tsx
@@ -1,38 +1,139 @@
-import { FC, useContext } from "react";
+import {
+  FC,
+  useContext,
+  useEffect,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "semantic-ui-react";
 import { SocketContext } from "../../SocketContext";
-import { ToTopPageButton } from "../utils/ToTopPageButton";
-import { GameThinking } from "../game/GameThinking";
+import { Canvas } from "@react-three/fiber";
+import { TopPagePreview } from "../createQuiz/TopPagePreview";
 
 export const TopPage: FC = () => {
   const navigate = useNavigate();
-  const { updateClientStatus, ping } = useContext(SocketContext);
+  const { updateClientStatus } = useContext(SocketContext);
+  const parentRef = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+    // @ts-ignore
+    window.particlesJS.load(
+      "particles-js",
+      "public/particles.json",
+      function () {
+        console.log("callback - particles.js config loaded");
+      },
+    );
+  }, []);
 
   return (
     <div
       style={{
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
+        position: "relative",
+        height: "100dvh",
+        width: "100dvw",
+        background: "#111",
       }}
+      ref={parentRef}
+      id="particles-js"
     >
-      <div>
-        <ToTopPageButton />
-        <div>This is a top page.</div>
-        <Button
-          onClick={() => {
-            updateClientStatus("NAME_INPUTING");
-            navigate("/game");
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100dvw",
+          height: "100dvh",
+          display: "flex",
+          flexDirection: "column",
+          maxWidth: "680px",
+          margin: "0 auto",
+          // backgroundColor: "111",
+          justifyContent: "space-around",
+          zIndex: 1,
+        }}
+      >
+        <div>&nbsp;</div>
+        <div>&nbsp;</div>
+        <div>&nbsp;</div>
+        <img
+          style={{
+            position: "absolute",
+            display: "inline-block",
+            top: "5px",
+            left: "5px",
+            transform: "rotate(-8deg)",
+            width: "80%",
+          }}
+          src="/public/logo.png"
+          alt="logo"
+        />
+
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "15px",
+            alignItems: "center",
           }}
         >
-          問題を解く
-        </Button>
-        <Button onClick={() => navigate("/create-quiz")}>問題を作る</Button>
-        <Button onClick={() => ping()}>ping</Button>
+          <Button
+            onClick={() => {
+              updateClientStatus("NAME_INPUTING");
+              navigate("/game");
+            }}
+            size="huge"
+            style={{
+              backgroundColor: "#A538EE",
+              color: "white",
+              borderRadius: "40px",
+              border: "3px solid white",
+              padding: "20px 30px",
+              fontWeight: "normal",
+              width: "73%",
+            }}
+          >
+            みんなでクイズをとく
+          </Button>
+          <Button
+            style={{
+              backgroundColor: "white",
+              color: "#A538EE",
+              borderRadius: "40px",
+              border: "2px solid #A538EE",
+              padding: "10px 30px",
+              fontWeight: "normal",
+              width: "73%",
+            }}
+            onClick={() => navigate("/create-quiz")}
+          >
+            クイズをつくる
+          </Button>
+        </div>
       </div>
-      <GameThinking />
+      <div
+        style={{
+          // border: "2px solid green",
+          position: "absolute",
+          top: "30dvh",
+          left: 0,
+          width: "100dvw",
+          height: "68dvh",
+          zIndex: "8",
+          opacity: "0.8",
+        }}
+      >
+        <Canvas
+          style={{ background: "transparent" }}
+          camera={{ position: [0, 2.8, 0.4] }}
+        >
+          <TopPagePreview />
+        </Canvas>
+      </div>
     </div>
   );
 };
