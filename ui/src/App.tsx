36d84f53b570b615a7c4bcb7bb36b044f02cf751
starmine7940 <starmine7940@gmail.com>
2024-08-31 01:06:06 +0900

    app.tsx に maxWidth を適用

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,12 +1,28 @@
+import { createUseStyles } from "react-jss";
 import { AppRoutes } from "./AppRoutes";
 import { SocketContextProvider } from "./SocketContext";
 
+const useStyles = createUseStyles({
+  app: {
+    display: "flex",
+    justifyContent: "center",
+    height: "100%",
+    width: "100%"
+  },
+  appChild: {
+    maxWidth: "680px",
+  },
+})
+
 function App() {
+  const classes = useStyles()
   return (
-    <div style={{ height: "100%", width: "100%" }}>
-      <SocketContextProvider>
-        <AppRoutes />
-      </SocketContextProvider>
+    <div className={classes.app}>
+      <div className={classes.appChild}>
+        <SocketContextProvider>
+          <AppRoutes />
+        </SocketContextProvider>
+      </div>
     </div>
   );
 }
