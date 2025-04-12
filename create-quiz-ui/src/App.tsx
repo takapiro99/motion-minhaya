bd3b0894e7ff270a356a6f71a6273cf4c7499e8f
starmine7940 <starmine7940@gmail.com>
2025-04-12 23:35:44 +0900

    思い出せない commit

--- create-quiz-ui/src/App.tsx
+++ create-quiz-ui/src/App.tsx
@@ -1,10 +1,28 @@
+import { createUseStyles } from "react-jss";
 import { CreateQuizPage } from "./components/CreateQuizPage";
 import "semantic-ui-css/semantic.min.css";
 
+const useStyles = createUseStyles({
+  app: {
+    display: "flex",
+    justifyContent: "center",
+    height: "100%",
+    width: "100%",
+    background: "#111",
+  },
+  appChild: {
+    maxWidth: "680px",
+    width: "100%",
+  },
+});
+
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App" style={{ height: "100dvh" }}>
-      <CreateQuizPage />
+    <div className={classes.app}>
+      <div className={classes.appChild}>
+        <CreateQuizPage />
+      </div>
     </div>
   );
 }
